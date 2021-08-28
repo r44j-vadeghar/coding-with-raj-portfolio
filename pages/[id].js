7a3a2fc2d4dd349c44055b2758ea40cc04689321
Raj Vadeghar <instigator0002@gmail.com>
2021-08-28 21:45:50 +0530

    debug

--- pages/[id].js
+++ pages/[id].js
@@ -123,8 +123,86 @@ export async function getServerSideProps(context) {
         { techImgSrc: "/tailwind-logo.svg" },
       ],
       demoLink:
-        "https://codingwithraj-todo-app-gbejf939x-rajvadeghar.vercel.app/",
+        "https://codingwithraj-todo-app-nzm33nrtb-rajvadeghar.vercel.app/",
       bgImg: "bg-todo-app",
+    },
+    {
+      id: "4",
+      projectName: "Instagram Redesign",
+      descriptionList: [
+        {
+          desc: "This is Instagram Redesign, Where we can get complete user experience like instagram",
+        },
+        {
+          desc: "CRUD operations are integrated",
+        },
+        {
+          desc: "Also, users can post images, comment etc",
+        },
+        {
+          desc: "Authentication feature is enabled (Register and login functionalities), Google Login and Facebook Login.",
+        },
+        {
+          desc: "Mobile responsive design.",
+        },
+      ],
+      technologiesUsed: [
+        { techImgSrc: "/react-logo.png" },
+        { techImgSrc: "/firebase-logo.png" },
+        { techImgSrc: "/materialui-logo.png" },
+        { techImgSrc: "/insta-logo.png" },
+      ],
+      demoLink: "https://instagram-redesign-4778b.web.app/",
+      bgImg: "bg-instagram-app",
+    },
+    {
+      id: "5",
+      projectName: "Covid Tracker",
+      descriptionList: [
+        {
+          desc: "This is Covid Tracker, Where we can track covid cases",
+        },
+        {
+          desc: "This is build using api from disease.sh",
+        },
+        {
+          desc: "Also, users can get track visualizing maps/graphs",
+        },
+        {
+          desc: "Mobile responsive design.",
+        },
+      ],
+      technologiesUsed: [
+        { techImgSrc: "/react-logo.png" },
+        { techImgSrc: "/firebase-logo.png" },
+        { techImgSrc: "/materialui-logo.png" },
+      ],
+      demoLink: "https://codingwithraj-covid-tracker.web.app/",
+      bgImg: "bg-covid-app",
+    },
+    {
+      id: "6",
+      projectName: "Netflix Clone",
+      descriptionList: [
+        {
+          desc: "This is Netflix Clone, Where we can get latest movies with categories",
+        },
+        {
+          desc: "This is build using api from TMDB",
+        },
+        {
+          desc: "Also, users can watch trailers by clicking any movie item. Although some may not work.",
+        },
+        {
+          desc: "Authentication feature is enabled (Register and login functionalities)",
+        },
+      ],
+      technologiesUsed: [
+        { techImgSrc: "/react-logo.png" },
+        { techImgSrc: "/netflix-logo.png" },
+      ],
+      demoLink: "https://codingwithraj-netflix-clone.web.app/",
+      bgImg: "bg-netflix-app",
     },
   ];
 
